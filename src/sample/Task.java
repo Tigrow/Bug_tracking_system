be5b7a1303965bb961abef400e46f121971098ba
Алексей Кислов <leha_kislo@mail.ru>
2018-06-30 12:57:38 +0300

    Add Task POJO

---
+++ src/sample/Task.java
@@ -0,0 +1,58 @@
+package sample;
+
+public class Task {
+    private int projectId;
+    private String Subject;
+    private String type;
+    private int priority;
+    private int user_id;
+    private String description;
+
+    public int getProjectId() {
+        return projectId;
+    }
+
+    public void setProjectId(int projectId) {
+        this.projectId = projectId;
+    }
+
+    public String getSubject() {
+        return Subject;
+    }
+
+    public void setSubject(String subject) {
+        Subject = subject;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
