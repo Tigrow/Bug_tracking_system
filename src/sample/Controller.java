ee7856bde83c0ba166a35114693417d4cae2690d
Алексей Кислов <leha_kislo@mail.ru>
2018-06-30 17:05:11 +0300

    refactor

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -5,9 +5,6 @@ import javafx.scene.input.KeyEvent;
 import javafx.stage.FileChooser;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Controller {
