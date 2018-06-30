2ebee527fda09d1813dddfccc699b9bda6e1a054
Алексей Кислов <leha_kislo@mail.ru>
2018-06-30 16:46:10 +0300

    add SQLite DB class

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,35 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Controller {
+    public void handleKeyInput(KeyEvent keyEvent) {
+        //
+    }
+
+    public void handleAboutAction(ActionEvent actionEvent) {
+    }
+
+    public void handleNewDBAction(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        //Set extension filter
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("DB files (*.DB)", "*.DB");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        //Show save file dialog
+        File file = fileChooser.showSaveDialog(null);
+        try {
+            DataBaseHandler.getInstance().CreateDB(file);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
