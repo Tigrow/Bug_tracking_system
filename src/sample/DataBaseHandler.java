1f76af9267580e4b3b6c175a6e752dc6a9b163e2
Алексей Кислов <leha_kislo@mail.ru>
2018-06-30 17:04:38 +0300

    added getStatement

--- src/sample/DataBaseHandler.java
+++ src/sample/DataBaseHandler.java
@@ -3,7 +3,7 @@ package sample;
 import java.io.File;
 import java.sql.*;
 
-public class DataBaseHandler {
+class DataBaseHandler {
     private static DataBaseHandler ourInstance = new DataBaseHandler();
 
     static DataBaseHandler getInstance() {
@@ -12,21 +12,23 @@ public class DataBaseHandler {
 
     private DataBaseHandler() {
     }
-    public static Connection conn;
-    private static Statement statmt;
 
     void CreateDB(File fileName) throws SQLException {
         String url = "jdbc:sqlite:" + fileName.getAbsolutePath();
 
         Connection conn = DriverManager.getConnection(url);
                 DatabaseMetaData meta = conn.getMetaData();
-                System.out.println("The driver name is " + meta.getDriverName());
-                System.out.println("A new database has been created.");
-                statmt = conn.createStatement();
+                Statement statmt = conn.createStatement();
                 statmt.execute("CREATE TABLE if not exists 'users' (" +
                         "'id' INTEGER PRIMARY KEY AUTOINCREMENT," +
-                        " 'name' text," +
-                        " 'priority' INTEGER" +
+                        " 'name' TEXT," +
+                        " 'priority' INTEGER," +
+                        " 'password' TEXT" +
                         ");");
+    }
+    Statement LoadDB(File fileName) throws SQLException {
+        String url = "jdbc:sqlite:" + fileName.getAbsolutePath();
+        Connection conn = DriverManager.getConnection(url);
+        return conn.createStatement();
     }
 }
