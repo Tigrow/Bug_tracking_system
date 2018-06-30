d6338c5a9572d9a47098f70727810483ce42377e
Алексей Кислов <leha_kislo@mail.ru>
2018-06-30 13:21:37 +0300

    add Project POJO

---
+++ src/sample/Project.java
@@ -0,0 +1,31 @@
+package sample;
+
+public class Project {
+    private int project;
+    private String name;
+    private String description;
+
+    public int getProject() {
+        return project;
+    }
+
+    public void setProject(int project) {
+        this.project = project;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
