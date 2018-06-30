3743bf53fc555e3e951289268010d405fededa45
Алексей Кислов <leha_kislo@mail.ru>
2018-06-30 16:49:10 +0300

    Add repository

---
+++ src/sample/Repository/Repository.java
@@ -0,0 +1,17 @@
+package sample.Repository;
+
+import java.util.List;
+
+public interface Repository<T> {
+    void add(T item);
+
+    void add(Iterable<T> items);
+
+    void update(T item);
+
+    void remove(T item);
+
+    void remove(String specification);
+
+    List<T> query(String specification);
+}
---
+++ src/sample/Repository/UserRepository.java
@@ -0,0 +1,37 @@
+package sample.Repository;
+
+import sample.User;
+
+import java.util.List;
+
+public class UserRepository implements Repository<User> {
+    @Override
+    public void add(User item) {
+
+    }
+
+    @Override
+    public void add(Iterable<User> items) {
+
+    }
+
+    @Override
+    public void update(User item) {
+
+    }
+
+    @Override
+    public void remove(User item) {
+
+    }
+
+    @Override
+    public void remove(String specification) {
+
+    }
+
+    @Override
+    public List<User> query(String specification) {
+        return null;
+    }
+}
