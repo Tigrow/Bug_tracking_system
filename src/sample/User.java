930c02552461bb2d36b42052f85fba0a704c13c5
Алексей Кислов <leha_kislo@mail.ru>
2018-07-01 16:24:02 +0300

    update User

--- src/sample/User.java
+++ src/sample/User.java
@@ -1,8 +1,40 @@
 package sample;
 
 public class User {
-    int id;
-    String Name;
-    int priority;
-    String password;
+    private int id;
+    private String Name;
+    private int priority;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
