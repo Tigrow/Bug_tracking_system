31ed5128ac82d1db779188c7399101e3e92da9ec
Алексей Кислов <leha_kislo@mail.ru>
2018-07-02 20:56:56 +0300

    add method toString

--- src/sample/User.java
+++ src/sample/User.java
@@ -37,4 +37,9 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
