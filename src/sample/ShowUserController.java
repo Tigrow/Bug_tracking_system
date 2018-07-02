b2473098f4aedd91b820a661dae75ad5ca358dfb
Алексей Кислов <leha_kislo@mail.ru>
2018-07-02 21:00:17 +0300

    add methods to Users view

--- src/sample/ShowUserController.java
+++ src/sample/ShowUserController.java
@@ -2,16 +2,57 @@ package sample;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import sample.Repository.UserRepository;
+
+import java.io.IOException;
 
 public class ShowUserController {
     @FXML
     private ListView<User> listView;
 
-    public void OnStart(UserRepository userRepository){
+    private UserRepository userRepository;
+    private Stage usersStage;
+
+    void OnStart(UserRepository userRepository, Stage usersStage){
+        this.usersStage = usersStage;
+        this.userRepository = userRepository;
         ObservableList<User> list = FXCollections.observableList(userRepository.query(""));
         listView.setItems(list);
+    }
+
+    protected void handleAddAction(ActionEvent actionEvent) {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("resources/adduser.fxml"));
+        GridPane page = null;
+        try {
+            page = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Stage dialogStage = new Stage();
+        dialogStage.setTitle("User");
+        dialogStage.initModality(Modality.WINDOW_MODAL);
+        dialogStage.initOwner(usersStage);
+        Scene scene = new Scene(page);
+        dialogStage.setScene(scene);
+
+        AddUserController controller = loader.getController();
+        //controller.OnStart(userRepository, dialogStage);
+        dialogStage.showAndWait();
+    }
+
+    protected void handleEditAction(ActionEvent actionEvent) {
+    }
+
+    protected void handleDeleteAction(ActionEvent actionEvent) {
     }
 }
