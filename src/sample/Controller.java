cabd604e7f11b409c64f34fd5b81b88f4d75814f
Алексей Кислов <leha_kislo@mail.ru>
2018-07-03 22:30:37 +0300

    add TaskRepository

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,26 +1,44 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import sample.Repository.UserRepository;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Controller {
 
+    @FXML
+    TabPane tabPane;
+
     private UserRepository userRepository;
+
+    public void loadView(){
+        File file = loadParams();
+        try {
+            userRepository = new UserRepository(file);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public void handleKeyInput(KeyEvent keyEvent) {
     }
+
 
     public void handleAboutAction(ActionEvent actionEvent) {
     }
@@ -31,6 +49,7 @@ public class Controller {
         File file = fileChooser.showOpenDialog(null);
         try {
             userRepository = new UserRepository(file);
+            saveParamChanges(file);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -75,4 +94,51 @@ public class Controller {
         }
     }
 
+    public File loadParams() {
+        Properties props = new Properties();
+        InputStream is = null;
+        try {
+            File file = new File("prop.properties");
+            is = new FileInputStream(file);
+        }
+        catch ( Exception e ) { is = null; }
+        try {
+            if (is == null) {
+                is = getClass().getResourceAsStream("prop.properties");
+            }
+            props.load(is);
+        }
+        catch ( Exception e ) { }
+        return new File(props.getProperty("lastDB", "/TEST.db"));
+    }
+
+    public void saveParamChanges(File file) {
+        try {
+            Properties props = new Properties();
+            props.setProperty("lastDB", ""+file.getAbsolutePath());
+            File f = new File("prop.properties");
+            OutputStream out = new FileOutputStream(f);
+        }
+        catch (Exception e ) {
+            e.printStackTrace();
+        }
+    }
+
+    public void handleAddProjectAction(Event event) {
+        addNewTab();
+    }
+
+    private void addNewTab(){
+        try {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("resources/projectTab.fxml"));
+        Tab tab = new Tab();
+        tab.setText("project");
+        HBox hbox = loader.load();
+        tab.setContent(hbox);
+        tabPane.getTabs().add(tab);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/sample/Repository/TaskRepository.java
@@ -0,0 +1,37 @@
+package sample.Repository;
+
+import sample.Task;
+
+import java.util.List;
+
+public class TaskRepository implements Repository<Task> {
+    @Override
+    public void add(Task item) {
+
+    }
+
+    @Override
+    public void add(Iterable<Task> items) {
+
+    }
+
+    @Override
+    public void update(Task item) {
+
+    }
+
+    @Override
+    public void remove(Task item) {
+
+    }
+
+    @Override
+    public void remove(String specification) {
+
+    }
+
+    @Override
+    public List<Task> query(String specification) {
+        return null;
+    }
+}
