02c85e8eec78f480e79d90256b29f26664514b4e
Алексей Кислов <leha_kislo@mail.ru>
2018-07-05 00:23:43 +0300

    refactor

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -15,6 +15,7 @@ import javafx.stage.FileChooser;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import sample.Repository.ProjectRepository;
+import sample.Repository.TaskRepository;
 import sample.Repository.UserRepository;
 
 import java.io.*;
@@ -29,47 +30,28 @@ public class Controller {
 
     private UserRepository userRepository;
     private ProjectRepository projectRepository;
+    private TaskRepository taskRepository;
     private Stage primaryStage;
     private boolean isFileDbLoaded = false;
     private boolean isUserLogined = false;
     private List<Project> projectList;
 
-    public void handleAboutAction(ActionEvent actionEvent) {
-    }
+
     public void handleLoadDBAction(ActionEvent actionEvent) {
         FileChooser fileChooser = new FileChooser();
         FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("DB files (*.DB)", "*.DB");
         fileChooser.getExtensionFilters().add(extFilter);
-        File file = fileChooser.showOpenDialog(null);
-        try {
-            if(file!=null){
-            userRepository = new UserRepository(file);
-            projectRepository = new ProjectRepository(file);
-            saveParamChanges(file);
-            setTitleName(file);
-            isFileDbLoaded = true;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        File file = fileChooser.showOpenDialog(primaryStage);
+        loadDataBase(file);
+
     }
 
     public void handleNewDBAction(ActionEvent actionEvent) {
         FileChooser fileChooser = new FileChooser();
         FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("DB files (*.DB)", "*.DB");
         fileChooser.getExtensionFilters().add(extFilter);
-        File file = fileChooser.showSaveDialog(null);
-        try {
-            if(file!=null){
-            DataBaseHandler.getInstance().CreateDB(file);
-            userRepository = new UserRepository(file);
-            projectRepository = new ProjectRepository(file);
-            saveParamChanges(file);
-            setTitleName(file);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        File file = fileChooser.showSaveDialog(primaryStage);
+        loadDataBase(file);
     }
 
     public void handleAddUserAction(ActionEvent actionEvent) {
@@ -83,11 +65,17 @@ public class Controller {
         addTabProject(project);
     }
 
-    private void showAddUserDialog(){
+    public void handleDeleteProjectAction(ActionEvent actionEvent) {
+        projectRepository.remove(projectList.get(tabPane.getSelectionModel().getSelectedIndex()));
+        projectList.remove(tabPane.getSelectionModel().getSelectedIndex());
+        tabPane.getTabs().remove(tabPane.getSelectionModel().getSelectedIndex());
+    }
+
+    private void showAddUserDialog() {
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(Main.class.getResource("resources/showUsersDialog.fxml"));
-            VBox page =  loader.load();
+            VBox page = loader.load();
 
             Stage dialogStage = new Stage();
             dialogStage.setTitle("User editor");
@@ -99,17 +87,36 @@ public class Controller {
             ShowUserController controller = loader.getController();
             controller.OnStart(userRepository, dialogStage);
             dialogStage.show();
-            //return null;//controller.isOkClicked();
         } catch (IOException e) {
             e.printStackTrace();
-            //return null;
         }
     }
 
-    private void setTitleName(File file){
-        primaryStage.setTitle("Bug Tracking System"+" [" +
+    private void setTitleName(File file) {
+        primaryStage.setTitle("Bug Tracking System" + " [" +
                 file.getAbsolutePath() +
                 "]");
+    }
+
+    private void loadDataBase(File file){
+        try {
+            if (file != null) {
+                DataBaseHandler.getInstance().CreateDB(file);
+                userRepository = new UserRepository(file);
+                projectRepository = new ProjectRepository(file);
+                taskRepository = new TaskRepository(file);
+                saveParamChanges(file);
+                setTitleName(file);
+                isFileDbLoaded = true;
+                tabPane.getTabs().clear();
+                projectList = projectRepository.query("");
+                for (Project aProjectList : projectList) {
+                    addTabProject(aProjectList);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     void loadParams(Stage primaryStage) {
@@ -119,54 +126,38 @@ public class Controller {
         try {
             File file = new File("prop.properties");
             is = new FileInputStream(file);
-            if (is == null) {
-                is = getClass().getResourceAsStream("prop.properties");
-            }
             props.load(is);
             File fileDB = new File(props.getProperty("lastDB", "/TEST.db"));
-            DataBaseHandler.getInstance().CreateDB(fileDB);
-            userRepository = new UserRepository(fileDB);
-            projectRepository = new ProjectRepository(fileDB);
-            projectList = projectRepository.query("");
-            for (Project aProjectList : projectList) {
-                addTabProject(aProjectList);
-            }
-            setTitleName(fileDB);
+            loadDataBase(fileDB);
+        } catch (Exception ignored) {
         }
-        catch ( Exception e ) { }
     }
 
     private void saveParamChanges(File file) {
         try {
             Properties props = new Properties();
-            props.setProperty("lastDB", ""+file.getAbsolutePath());
+            props.setProperty("lastDB", "" + file.getAbsolutePath());
             File f = new File("prop.properties");
             OutputStream out = new FileOutputStream(f);
-        }
-        catch (Exception e ) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void addTabProject(Project project){
+    private void addTabProject(Project project) {
         try {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(Main.class.getResource("resources/projectTab.fxml"));
-        Tab tab = new Tab();
-        HBox hbox = loader.load();
-        tab.setContent(hbox);
-        tabPane.getTabs().add(tab);
-        tabPane.getSelectionModel().select(tab);
-        ProjectController projectController = loader.getController();
-        projectController.setData(tab,project,projectRepository);
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(Main.class.getResource("resources/projectTab.fxml"));
+            Tab tab = new Tab();
+            HBox hbox = loader.load();
+            tab.setContent(hbox);
+            tabPane.getTabs().add(tab);
+            tabPane.getSelectionModel().select(tab);
+            ProjectController projectController = loader.getController();
+            projectController.setData(tab, project, projectRepository);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void handleDeleteProjectAction(ActionEvent actionEvent) {
-        projectRepository.remove(projectList.get(tabPane.getSelectionModel().getSelectedIndex()));
-        projectList.remove(tabPane.getSelectionModel().getSelectedIndex());
-        tabPane.getTabs().remove(tabPane.getSelectionModel().getSelectedIndex());
-    }
 }
